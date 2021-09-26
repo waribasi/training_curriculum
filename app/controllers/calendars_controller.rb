68f8322241e0939f07dfa9c41bb2edff6230ca25
waribasi <kakuremafu@icloud.com>
2021-09-26 18:40:12 +0900

    再々修正しました

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -20,7 +20,6 @@ class CalendarsController < ApplicationController
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
@@ -35,14 +34,15 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = []
-      if 
+      # wday_num = (今日の日付の曜日の数字) + (１ずつ加算する)
+      wday_num = @todays_date.wday + x
+      if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date).day, :plans => today_planswday, wday => wdays_num }
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      # 配列wdaysから曜日を取得する。→配列から値を取得する記述は？　wdays[○]
       @week_days.push(days)
     end
-
   end
 end
